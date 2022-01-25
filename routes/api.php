eab533e51586b9f74dbf31746f2167f4dc97a977
Admin 水野 <mizno911.zeta@gmail.com>
2022-01-25 18:58:45 +0900

    ログイン時のみ利用できるapiの作成

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,11 @@ use App\Http\Controllers\TodoController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::prefix('todo')->group(function () {
         Route::get('/all', [TodoController::class, 'all']);
         Route::post('/', [TodoController::class, 'create']);
